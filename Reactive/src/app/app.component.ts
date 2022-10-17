346a420eaab372404ecd98a64db4365507fa3db9
SANADASHRAF <76807340+SANADASHRAF@users.noreply.github.com>
2022-10-17 19:18:22 +0200

    Html form ,form model, form control and formgroup ,FormBuilder

--- Reactive/src/app/app.component.ts
+++ Reactive/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+// import {  FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Reactive';
+constructor(private fb:FormBuilder){}
+
+
+  //MODEL using formgroup and form control
+  // registrationform =new FormGroup({
+  //   username:new FormControl(''),
+  //   password:new FormControl(''),
+  //   confirmpassword:new FormControl(''),
+
+  // })
+
+
+  //MODEL using formbuilder 
+  registrationform=this.fb.group({
+    username:[''],
+    password:[''],
+    confirmpassword:[''],
+
+  })
+
+
+  //api to set value to input parameter
+  onsubmit(){
+    //or patchvalue
+    this.registrationform.setValue({
+      username:"sanad",
+      password:'123',
+      confirmpassword:'123'
+    })
+  }
 }
